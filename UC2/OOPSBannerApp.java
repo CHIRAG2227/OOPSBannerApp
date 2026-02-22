37c8fa9b58b1d576d59341da442a4d43575ead0a
Chirag Agarwal <ca9010@srmist.edu.in>
2026-02-23 00:45:17 +0530

    UC2 - Added 7-line ASCII Banner for OOPS

--- UC2/OOPSBannerApp.java
+++ UC2/OOPSBannerApp.java
@@ -1,13 +1,13 @@
-/**
- * OOPSBannerApp
- * Use Case 1 - Prints literal text "OOPS"
- *
- * @author Chirag Agarwal
- * @version 1.0
- */
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" *****    *****    ******    ***** ");
+        System.out.println("*     *  *     *   *     *  *     *");
+        System.out.println("*     *  *     *   *     *  *      ");
+        System.out.println("*     *  *     *   ******    ***** ");
+        System.out.println("*     *  *     *   *              *");
+        System.out.println("*     *  *     *   *        *     *");
+        System.out.println(" *****    *****    *         ***** ");
+
     }
 }
