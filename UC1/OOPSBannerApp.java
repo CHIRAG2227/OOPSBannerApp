854710e0f501cecd1dfe8cc79be5e88fd737a6e5
Chirag Agarwal <ca9010@srmist.edu.in>
2026-02-23 01:33:14 +0530

    UC4 - Implemented Banner using Array and Enhanced For Loop

--- UC1/OOPSBannerApp.java
+++ UC1/OOPSBannerApp.java
@@ -1,5 +1,15 @@
+/**
+ * OOPSBannerApp
+ * Use Case 1 - Print "OOPS"
+ * 
+ * @author Chirag
+ * @version 1.0
+ */
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
+
         System.out.println("OOPS");
+
     }
 }
--- UC2/OOPSBannerApp.java
+++ UC2/OOPSBannerApp.java
@@ -1,13 +1,21 @@
+/**
+ * OOPSBannerApp
+ * UC2 - Display "OOPS" as ASCII Banner using '*' and spaces
+ *
+ * @author Chirag Agarwal
+ * @version 2.0
+ */
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
 
-        System.out.println(" *****    *****    ******    ***** ");
-        System.out.println("*     *  *     *   *     *  *     *");
-        System.out.println("*     *  *     *   *     *  *      ");
-        System.out.println("*     *  *     *   ******    ***** ");
-        System.out.println("*     *  *     *   *              *");
-        System.out.println("*     *  *     *   *        *     *");
-        System.out.println(" *****    *****    *         ***** ");
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     *  *****   ***** ");
+        System.out.println("*     * *     *       *       *");
+        System.out.println("*     * *     *       *       *");
+        System.out.println(" *****   *****   *****   ***** ");
 
     }
 }
